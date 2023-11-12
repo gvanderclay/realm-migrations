953b24ddcccdb711effa51aaa93717ca59c7e305
Gage Vander Clay <gvanderclay@gmail.com>
2023-11-12 14:39:23 -0600

    Add comments

--- src/db/realm.ts
+++ src/db/realm.ts
@@ -9,9 +9,14 @@ import { schema } from "./schema";
 export const createRealm = () => {
   try {
     const realm = new Realm({
+      // Array of schema records to use in the Realm
       schema: schema,
+      // Using in memory realm for demonstration purposes
       inMemory: true,
+      // Utilize the migration runner with the migrations that are generated
+      // by the migration generator
       onMigration: buildMigrationRunner(migrations),
+      // Set the schema version to the length of the migrations array
       schemaVersion: migrations.length,
     });
     initializeMigrationRecordsIfNecessary(realm);
